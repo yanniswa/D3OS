f562bcbe2565c618e9f131cb9f531a1b61e35a42
yanniswa <yanniswagner@web.de>
2026-02-20 15:26:22 +0100

    removed unnecessary method declaration, leftover from old design

--- os/library/rpc/src/pipe_transport.rs
+++ os/library/rpc/src/pipe_transport.rs
@@ -1,8 +1,6 @@
-#![allow(unused_imports)]
-
 extern crate alloc;
 
-use crate::consts::{CLIENT_CLOSE_DELAY_MS, PIPE_BUF};
+use crate::consts::CLIENT_CLOSE_DELAY_MS;
 use crate::error::RpcError;
 use crate::io_helpers::{read_raw_bytes_from_pipe, write_exact};
 use crate::transport::Transport;
@@ -19,54 +17,34 @@ impl PipeTransport {
     }
 }
 
-fn writer_thread(path: &str, msg: &[u8]) -> Option<Result<(), RpcError>> {
-    let thread = thread::current().unwrap();
-    let pid = concurrent::process::current().map(|p| p.id()).unwrap_or(0);
-    debug!("writer_thread (pid={} tid={}): start, path={}, msg.len()={}", pid, thread.id(), path, msg.len());
-
-    let res = open(path, OpenOptions::WRITEONLY);
-    if res.is_err() {
-        error!("writer_thread (pid={} tid={}): open failed, error: {:?}", pid, thread.id(), res);
-        return Some(Err(RpcError::PipeOpenFailed));
-    }
-    let fh = res.unwrap();
-    debug!("writer_thread (pid={} tid={}): opened fh={}", pid, thread.id(), fh);
-
-    // msg already contains the raw Cap'n Proto framing produced by
-    // serialize::write_message_to_words in the caller.  We write it as-is
-    // so both sides share the same framing without an extra length prefix.
-    let full_buf = msg;
-
-    if let Err(e) = write_exact(fh, full_buf) {
-        error!("writer_thread (pid={} tid={}): write_exact failed: {:?}", pid, thread.id(), e);
-        let _ = close(fh);
-        return Some(Err(e));
-    }
-
-    debug!(
-        "writer_thread (pid={} tid={}): send complete, {} bytes written",
-        pid,
-        thread.id(),
-        full_buf.len()
-    );
-
-    // TODO: Remove this sleep workaround. This is a race condition fix
-    // that should be replaced with proper pipe-close protocol or ACK mechanism.
-    thread::sleep(CLIENT_CLOSE_DELAY_MS);
-    match close(fh) {
-        Ok(_) => debug!("writer_thread: closed fh={}", fh),
-        Err(e) => error!("writer_thread: close failed fh={} err={:?}", fh, e),
-    }
-
-    None
-}
-
 impl Transport for PipeTransport {
     fn send(&self, path: &str, msg: &[u8]) -> Result<(), RpcError> {
-        // Perform the write from this thread (synchronous). If the writer
-        // encounters an error, propagate it as Err(RpcError).
-        if let Some(res) = writer_thread(path, msg) {
-            return res;
+        let thread = thread::current().unwrap();
+        let pid = concurrent::process::current().map(|p| p.id()).unwrap_or(0);
+        debug!("send (pid={} tid={}): start, path={}, msg.len()={}", pid, thread.id(), path, msg.len());
+
+        let res = open(path, OpenOptions::WRITEONLY);
+        if res.is_err() {
+            error!("send (pid={} tid={}): open failed, error: {:?}", pid, thread.id(), res);
+            return Err(RpcError::PipeOpenFailed);
+        }
+        let fh = res.unwrap();
+        debug!("send (pid={} tid={}): opened fh={}", pid, thread.id(), fh);
+
+        if let Err(e) = write_exact(fh, msg) {
+            error!("send (pid={} tid={}): write_exact failed: {:?}", pid, thread.id(), e);
+            let _ = close(fh);
+            return Err(e);
+        }
+
+        debug!("send (pid={} tid={}): send complete, {} bytes written", pid, thread.id(), msg.len());
+
+        // TODO: Remove this sleep workaround. This is a race condition fix
+        // that should be replaced with proper pipe-close protocol or ACK mechanism.
+        thread::sleep(CLIENT_CLOSE_DELAY_MS);
+        match close(fh) {
+            Ok(_) => debug!("send: closed fh={}", fh),
+            Err(e) => error!("send: close failed fh={} err={:?}", fh, e),
         }
 
         Ok(())
